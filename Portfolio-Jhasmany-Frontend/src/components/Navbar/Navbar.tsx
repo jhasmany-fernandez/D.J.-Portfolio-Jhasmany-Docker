25bec62d97d6c93fb34c8f0b2dee9badfa25be1d
jhasmany-fernandez <jhasmany.fernandez.dev@gmail.com>
2025-10-09 06:35:53 -0400

    barra de navegacion oculto para ciertos rutas

--- Portfolio-Jhasmany-Frontend/src/components/Navbar/Navbar.tsx
+++ Portfolio-Jhasmany-Frontend/src/components/Navbar/Navbar.tsx
@@ -32,6 +32,7 @@ const navItems = [
 const Navbar = () => {
   const [isVisible, setIsVisible] = useState(false)
   const pathname = usePathname()
+  const shouldHideNavItems = pathname === '/auth/login' || pathname === '/auth/register' || pathname === '/newsletter/subscribe'
 
   const toggleMenu = () => {
     setIsVisible(!isVisible)
@@ -51,31 +52,35 @@ const Navbar = () => {
           </Link>
         )}
 
-        <div className="md:hidden">
-          <button onClick={toggleMenu}>
-            {isVisible ? (
-              <CloseIcon className="text-primary-content" />
-            ) : (
-              <BurgerIcon className="text-primary-content" />
-            )}
-          </button>
-        </div>
+        {!shouldHideNavItems && (
+          <div className="md:hidden">
+            <button onClick={toggleMenu}>
+              {isVisible ? (
+                <CloseIcon className="text-primary-content" />
+              ) : (
+                <BurgerIcon className="text-primary-content" />
+              )}
+            </button>
+          </div>
+        )}
 
-        <ul
-          className={`${isVisible ? 'flex' : 'hidden'} animate-fade-in bg-primary absolute top-16 left-0 z-10 h-dvh w-dvw flex-col md:static md:top-0 md:flex md:h-full md:w-[72%] md:flex-row lg:w-[70%]`}>
-          {navItems.map(({ label, href }) => (
-            <li
-              key={href}
-              onClick={() => setIsVisible(false)}
-              className="border-border flex items-center border-b px-4 text-2xl md:border-y-0 md:border-e md:text-base md:first:border-s md:last:ml-auto md:last:border-none md:last:px-0 lg:px-8">
-              <Link
-                href={href}
-                className={`text-primary-content hover:text-neutral w-full py-7 transition-all duration-150 md:py-0 ${pathname === href ? 'text-neutral cursor-text' : ''}`}>
-                {label}
-              </Link>
-            </li>
-          ))}
-        </ul>
+        {!shouldHideNavItems && (
+          <ul
+            className={`${isVisible ? 'flex' : 'hidden'} animate-fade-in bg-primary absolute top-16 left-0 z-10 h-dvh w-dvw flex-col md:static md:top-0 md:flex md:h-full md:w-[72%] md:flex-row lg:w-[70%]`}>
+            {navItems.map(({ label, href }) => (
+              <li
+                key={href}
+                onClick={() => setIsVisible(false)}
+                className="border-border flex items-center border-b px-4 text-2xl md:border-y-0 md:border-e md:text-base md:first:border-s md:last:ml-auto md:last:border-none md:last:px-0 lg:px-8">
+                <Link
+                  href={href}
+                  className={`text-primary-content hover:text-neutral w-full py-7 transition-all duration-150 md:py-0 ${pathname === href ? 'text-neutral cursor-text' : ''}`}>
+                  {label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </nav>
   )
